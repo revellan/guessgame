54168645a7670fa8bbc522ca9826cf0aeaf0c9ac
revellan <climb.simonschmidt@web.de>
2025-07-21 16:14:07 +0200

    Fix some Errors

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,18 @@ fn main() {
     let mut attempts: u32 = 1;
     let secret_number = rand::rng().random_range(1..=100);
     loop {
-        let guess = get_user_num();
-        let res = compare(&guess, &secret_number);
+        let mut guess: i32;
+        loop {
+            let guess_str = get_user_num();
+            match guess_str.trim().parse() {
+                Ok(num) => {
+                    guess = num;
+                    break;
+                }
+                Err(_) => continue,
+            }
+        }
+        let res = compare(&mut guess, &secret_number);
         if res == 1 {
             attempts = repeat("Too small!", attempts);
         } else if res == 2 {
@@ -33,13 +43,12 @@ fn compare(guessed_num: &i32, secret_num: &i32) -> i32 {
         Ordering::Greater => return 2,
     }
 }
-fn get_user_num() -> i32 {
+fn get_user_num() -> String {
     print!("Guess a Number: ");
     io::stdout().flush().expect("Unable to flush stdout");
     let mut return_value = String::new();
     io::stdin()
         .read_line(&mut return_value)
         .expect("Failed to read line!");
-    let return_value: i32 = return_value.trim().parse().expect("Enter a valid Integer!");
     return return_value;
 }
